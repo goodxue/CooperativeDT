03d649a488c636141e8f43546d89d3cd7f277d73
goodxue <627807499@qq.com>
2021-03-13 21:00:16 +0800

    carla_ros init

---
+++ carla_ros/spawn_vehicals.py
@@ -0,0 +1,221 @@
+import glob
+import os
+import sys
+import time
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+from carla import VehicleLightState as vls
+
+import argparse
+import logging
+from numpy import random
+
+VIEW_FOV = 90
+BB_COLOR = (255, 0, 0)
+car_num=[260,180,100]
+all_weather=[carla.WeatherParameters(cloudiness=60,sun_altitude_angle=-90,sun_azimuth_angle=150,
+                fog_density=20,fog_falloff=2,wetness=30),
+                carla.WeatherParameters(cloudiness=60,sun_altitude_angle=0,sun_azimuth_angle=150,
+                fog_density=22,fog_falloff=2,wetness=40),
+                carla.WeatherParameters(cloudiness=60,sun_altitude_angle=2,sun_azimuth_angle=90,
+                fog_density=10,fog_falloff=0,wetness=20),
+                carla.WeatherParameters(cloudiness=60,sun_altitude_angle=20,sun_azimuth_angle=120,
+                fog_density=0,fog_falloff=0,wetness=0),
+                carla.WeatherParameters(cloudiness=60,sun_altitude_angle=40,sun_azimuth_angle=180,
+                fog_density=0,fog_falloff=0,wetness=0),
+                carla.WeatherParameters(cloudiness=60,sun_altitude_angle=80,sun_azimuth_angle=280,
+                fog_density=0,fog_falloff=0,wetness=0)]
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+    argparser.add_argument(
+        '-n', '--number-of-vehicles',
+        metavar='N',
+        default=260,
+        type=int,
+        help='number of vehicles (default: 10)')
+    argparser.add_argument(
+        '-w', '--number-of-walkers',
+        metavar='W',
+        default=0,
+        type=int,
+        help='number of walkers (default: 50)')
+    argparser.add_argument(
+        '--safe',
+        action='store_true',
+        help='avoid spawning vehicles prone to accidents')
+    argparser.add_argument(
+        '--filterv',
+        metavar='PATTERN',
+        default='vehicle.*',
+        help='vehicles filter (default: "vehicle.*")')
+    argparser.add_argument(
+        '--filterw',
+        metavar='PATTERN',
+        default='walker.pedestrian.*',
+        help='pedestrians filter (default: "walker.pedestrian.*")')
+    argparser.add_argument(
+        '--tm-port',
+        metavar='P',
+        default=8000,
+        type=int,
+        help='port to communicate with TM (default: 8000)')
+    argparser.add_argument(
+        '--sync',
+        action='store_true',
+        help='Synchronous mode execution')
+    argparser.add_argument(
+        '--hybrid',
+        action='store_true',
+        help='Enanble')
+    argparser.add_argument(
+        '-s', '--seed',
+        metavar='S',
+        type=int,
+        help='Random device seed')
+    argparser.add_argument(
+        '--car-lights-on',
+        action='store_true',
+        default=False,
+        help='Enanble car lights')
+    argparser.add_argument(
+        '--weather_num',
+        type=int,
+        default=0,
+        help='choose the weather')
+    args = argparser.parse_args()
+
+    client = carla.Client('localhost', 2000,1)
+    client.set_timeout(10.0)
+    world = client.load_world('Town05')
+    world.set_weather(all_weather[args.weather_num])
+
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    vehicles_list = []
+    synchronous_master = False
+    random.seed(args.seed if args.seed is not None else int(time.time()))
+
+    try:
+        world = client.get_world()
+        traffic_manager = client.get_trafficmanager(args.tm_port)
+        traffic_manager.set_global_distance_to_leading_vehicle(1.0)
+        if args.hybrid:
+            traffic_manager.set_hybrid_physics_mode(True)
+        if args.seed is not None:
+            traffic_manager.set_random_device_seed(args.seed)
+
+
+        if args.sync:
+            settings = world.get_settings()
+            traffic_manager.set_synchronous_mode(True)
+            if not settings.synchronous_mode:
+                synchronous_master = True
+                settings.synchronous_mode = True
+                settings.fixed_delta_seconds = 0.05
+                world.apply_settings(settings)
+            else:
+                synchronous_master = False
+
+        blueprints = world.get_blueprint_library().filter(args.filterv)
+        blueprints = [x for x in blueprints if int(x.get_attribute('number_of_wheels'))>=4]
+
+        # import pdb; pdb.set_trace()
+        if args.safe:
+            blueprints = [x for x in blueprints if int(x.get_attribute('number_of_wheels')) == 4]
+            # blueprints = [x for x in blueprints if not x.id.endswith('isetta')]
+            # blueprints = [x for x in blueprints if not x.id.endswith('carlacola')]
+            # blueprints = [x for x in blueprints if not x.id.endswith('cybertruck')]
+            # blueprints = [x for x in blueprints if not x.id.endswith('t2')]
+
+        blueprints = sorted(blueprints, key=lambda bp: bp.id)
+
+        spawn_points = world.get_map().get_spawn_points()
+        number_of_spawn_points = len(spawn_points)
+        args.number_of_vehicles=car_num
+        if args.number_of_vehicles < number_of_spawn_points:
+            random.shuffle(spawn_points)
+        elif args.number_of_vehicles > number_of_spawn_points:
+            msg = 'requested %d vehicles, but could only find %d spawn points'
+            logging.warning(msg, args.number_of_vehicles, number_of_spawn_points)
+            args.number_of_vehicles = number_of_spawn_points
+
+        # @todo cannot import these directly.
+        SpawnActor = carla.command.SpawnActor
+        SetAutopilot = carla.command.SetAutopilot
+        SetVehicleLightState = carla.command.SetVehicleLightState
+        FutureActor = carla.command.FutureActor
+
+        # --------------
+        # Spawn vehicles
+        # --------------
+        batch = []
+        for n, transform in enumerate(spawn_points):
+            if n >= args.number_of_vehicles:
+                break
+            blueprint = random.choice(blueprints)
+            if blueprint.has_attribute('color'):
+                color = random.choice(blueprint.get_attribute('color').recommended_values)
+                blueprint.set_attribute('color', color)
+            if blueprint.has_attribute('driver_id'):
+                driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
+                blueprint.set_attribute('driver_id', driver_id)
+            blueprint.set_attribute('role_name', 'autopilot')
+
+            # prepare the light state of the cars to spawn
+            light_state = vls.NONE
+            if args.car_lights_on:
+                light_state = vls.Position | vls.LowBeam | vls.LowBeam
+
+            # spawn the cars and set their autopilot and light state all together
+            batch.append(SpawnActor(blueprint, transform)
+                .then(SetAutopilot(FutureActor, True, traffic_manager.get_port()))
+                .then(SetVehicleLightState(FutureActor, light_state)))
+
+        for response in client.apply_batch_sync(batch, synchronous_master):
+            if response.error:
+                logging.error(response.error)
+            else:
+                vehicles_list.append(response.actor_id)
+        
+        traffic_manager.global_percentage_speed_difference(30.0)
+
+        while True:
+            if args.sync and synchronous_master:
+                world.tick()
+            else:
+                world.wait_for_tick()
+    finally:
+
+        if args.sync and synchronous_master:
+            settings = world.get_settings()
+            settings.synchronous_mode = False
+            settings.fixed_delta_seconds = None
+            world.apply_settings(settings)
+
+        print('\ndestroying %d vehicles' % len(vehicles_list))
+        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
+
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('\ndone.')
+    
